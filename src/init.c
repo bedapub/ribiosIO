98ed9d21cfb26bf1013bd2d11ad6d54ea88689c7
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-04-06 15:37:18 +0200

    add CALLMETHOD_DEF, and remove ribiosUtils parts

--- src/init.c
+++ src/init.c
@@ -1,27 +1,19 @@
-#include "ribios_utils.h"
+#include "ribios_io.h"
+#include <R_ext/Rdynload.h>
 
-#include <R_ext/Rdynload.h> /* DllInfo is needed to register the routines*/
-#include <R_ext/Visibility.h>
+#define CALLMETHOD_DEF(fun, numArgs) {#fun, (DL_FUNC) &(fun), (numArgs)}
 
-#include "r_trim.h"
-#include "r_mmatch.h"
-#include "r_endec.h"
-
-#include "log.h"
-
-static R_CallMethodDef callMethods[] = {
-  {"pwdecode", (DL_FUNC) &pwdecode, 1},
-  {"mmatch", (DL_FUNC) &mmatch, 3},
-  {"trim", (DL_FUNC) &trim, 3},
+static const R_CallMethodDef callMethods[] = {
+  CALLMETHOD_DEF(c_read_gct, 3),
+  CALLMETHOD_DEF(c_read_gmt, 1),
+  CALLMETHOD_DEF(c_write_gmt, 2),
+  CALLMETHOD_DEF(c_read_chip, 1),
+  CALLMETHOD_DEF(c_read_biokit_exprs, 1),
   {NULL, NULL, 0}
 };
 
-void R_init_ribiosUtils(DllInfo *info) {
-  R_registerRoutines(info, 
-		  NULL /*.C*/, 
-		  callMethods /*.Call*/, 
-		  NULL /*.Fortran*/, 
-		  NULL /*.External*/);
+void R_init_ribiosIO(DllInfo *info) {
+  R_registerRoutines(info, NULL, callMethods, NULL, NULL);
   /* the line below says that the DLL is not to be searched
    * for entry points specified by character strings so
    * .C etc calls will only find registered symbols
@@ -33,19 +25,4 @@ void R_init_ribiosUtils(DllInfo *info) {
    */ 
   R_forceSymbols(info, TRUE);
 
-  /* C functions implemented in ribiosUtils to be exported*/
-  // Required by ribiosArg
-  // arg_init
-  // arg_isInit
-  // arg_getPos
-  // arg_present
-  // strReplace
-  // usage
-  R_RegisterCCallable("ribiosUtils", "arg_init", (DL_FUNC) &arg_init);
-  R_RegisterCCallable("ribiosUtils", "arg_isInit", (DL_FUNC) &arg_isInit);
-  R_RegisterCCallable("ribiosUtils", "arg_getPos", (DL_FUNC) &arg_getPos);
-  R_RegisterCCallable("ribiosUtils", "arg_present", (DL_FUNC) &arg_present);
-  R_RegisterCCallable("ribiosUtils", "strReplace", (DL_FUNC) &strReplace);
-  R_RegisterCCallable("ribiosUtils", "usage", (DL_FUNC) &usage);
-  R_RegisterCCallable("ribiosUtils", "hlr_callocs", (DL_FUNC) &hlr_callocs);
 }
