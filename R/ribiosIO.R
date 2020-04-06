cead356510e09c175eac02eb8bcbdc953b4fe363
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-04-06 14:12:09 +0200

    add ribiosIO package

---
+++ R/ribiosIO.R
@@ -0,0 +1,7 @@
+#' High-performance file input/output with ribios
+#' @docType package
+#' @description Provides high-performance file input/output for computational biology tasks
+#' @useDynLib ribiosIO, .registration=TRUE, .fixes="C_"
+#' @name ribiosIO-package
+NULL
+
