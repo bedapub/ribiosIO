7e0f696a3e2f231ef1050b29ff9dffd7d43a4948
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-04-06 14:40:37 +0200

    remove redundant comments, and remove the RegisterCCable section, which should be rather in ribiosUtils

--- src/init.c
+++ src/init.c
@@ -14,31 +14,6 @@ static const R_CallMethodDef callMethods[] = {
 
 void R_init_ribiosIO(DllInfo *info) {
   R_registerRoutines(info, NULL, callMethods, NULL, NULL);
-  /* the line below says that the DLL is not to be searched
-  * for entry points specified by character strings so
-  * .C etc calls will only find registered symbols
-  */
   R_useDynamicSymbols(info, FALSE);
-  /* R_forceSymbols call only allows .C etc calls which
-   * specify entry points by R objects such as C_routineName
-   * (and not by character strings)
-   */
   R_forceSymbols(info, TRUE);
-
-  /* C functions implemented in ribiosUtils to be exported*/
-  // Required by ribiosArg
-  // arg_init
-  // arg_isInit
-  // arg_getPos
-  // arg_present
-  // strReplace
-  // usage
-  R_RegisterCCallable("ribiosUtils", "arg_init", (DL_FUNC) &arg_init);
-  R_RegisterCCallable("ribiosUtils", "arg_isInit", (DL_FUNC) &arg_isInit);
-  R_RegisterCCallable("ribiosUtils", "arg_getPos", (DL_FUNC) &arg_getPos);
-  R_RegisterCCallable("ribiosUtils", "arg_present", (DL_FUNC) &arg_present);
-  R_RegisterCCallable("ribiosUtils", "strReplace", (DL_FUNC) &strReplace);
-  R_RegisterCCallable("ribiosUtils", "usage", (DL_FUNC) &usage);
-  R_RegisterCCallable("ribiosUtils", "hlr_callocs", (DL_FUNC) &hlr_callocs);
-
 }
